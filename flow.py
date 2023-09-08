5e4cef5f404a6840e0a64f2294925db3061b6d89
JBinkowski <jan.binkowski96@gmail.com>
2023-09-08 15:04:14 +0200

    Lints

--- flow.py
+++ flow.py
@@ -1,17 +1,18 @@
-from urllib.request import urlretrieve
-from subprocess import call
-from os.path import join, exists
+import sys
+from glob import glob
 from os import makedirs
-import requests
+from os.path import join, exists
+from pathlib import Path
+from subprocess import call
+from typing import Union
+from urllib.request import urlretrieve
 
 import pandas as pd
-from glob import glob
+import requests
 from prefect import task, flow
-from pathlib import Path
 
-from typing import Union
-from src.utils import show_progress
 from src.exceptions import WrongGenomeAssembly, WrongSignalType, DataNotFound
+from src.utils import show_progress
 
 
 @task
@@ -23,11 +24,11 @@ def extract_files_from_experiments(
     for experiment in experiments:
         print(f"Parsing data from {experiment}.")
         endpoint = f"https://www.encodeproject.org/experiment/{experiment}/?format=json"
-        response = requests.get(endpoint).json()
-        
+        response = requests.get(endpoint, timeout=1000).json()
+
         summary = response["biosample_summary"]
         target = response["target"]["name"]
-        
+
         assay_type = response["assay_term_name"]
         cell_classification = response["biosample_ontology"]["classification"]
         term_name = response["biosample_ontology"]["term_name"]
@@ -67,7 +68,7 @@ def download_bigwig(file: str, link: str, output: str) -> None:
 
     endpoint = "https://www.encodeproject.org" + link
     output = join(output, f"{file}.bigWig")
-    
+
     if exists(output):
         print(f"File {file} already in {output} directory, skipping.")
     else:
@@ -83,9 +84,11 @@ def start_integration(
     genome_assembly: str = "GRCh38",
 ) -> None:
     if signal_type not in ["signal p-value", "fold change over control"]:
+        print("Signal type should be 'signal p-value' or 'fold change over control'!")
         raise WrongSignalType
 
     if genome_assembly not in ["GRCh38", "hg19"]:
+        print("Genome_assembly should be 'GRCh38' or 'hg19'!")
         raise WrongGenomeAssembly
 
     if not exists(output):
@@ -103,68 +106,105 @@ def start_integration(
     for file_id, file_link in zip(sample_sheet["File"], sample_sheet["Link"]):
         download_bigwig(file_id, file_link, join(output, "data"))
 
+
 @task(log_prints=True)
-def build_summary_matrix(bigwig_files: Union[list, tuple], bed_files: Union[list, tuple], output: str, workers: int) -> None:
+def build_summary_matrix(
+    bigwig_files: Union[list, tuple],
+    bed_files: Union[list, tuple],
+    output: str,
+    workers: int,
+) -> None:
     output_matrix = join(output, "summary.npz")
     output_summary = join(output, "summary.tsv")
-    
+
     bigwig_files = " ".join(bigwig_files)
     bed_files = " ".join(bed_files)
-        
+
     command = f"multiBigwigSummary BED-file -p {workers} -b {bigwig_files} --BED {bed_files} -o {output_matrix} --smartLabels --outRawCounts {output_summary}"
-    
+
     print(f"Running: {command}")
     call(command, shell=True)
 
+
 @task(log_prints=True)
-def compute_matrix(sample_sheet: pd.DataFrame, bigwig_files: Union[list, tuple], bed_files: Union[list, tuple], window: int, output: str, workers: int) -> None:
+def compute_matrix(
+    sample_sheet: pd.DataFrame,
+    bigwig_files: Union[list, tuple],
+    bed_files: Union[list, tuple],
+    window: int,
+    output: str,
+    workers: int,
+) -> None:
     files_ids = [Path(file).name.split(".")[0] for file in bigwig_files]
     labels = sample_sheet[sample_sheet.File.isin(files_ids)]["Target"].tolist()
     labels = " ".join(labels)
-    
+
     output = join(output, "matrix")
-    
+
     if exists(output):
-        print(f"Attention! Matrix already exists in this directory. Aborting to prevent overwriting!")
-        return ""
-    
+        print(
+            "Attention! Matrix already exists in this directory. Aborting to prevent overwriting!"
+        )
+        sys.exit()
+
     bigwig_files = " ".join(bigwig_files)
     bed_files = " ".join(bed_files)
     command = f"computeMatrix reference-point -p {workers} -S {bigwig_files} -R {bed_files} -a {window} -b {window} -o {output} --samplesLabel {labels}"
-    
+
     print(f"Running: {command}")
     call(command, shell=True)
-    
+
+
 @task(log_prints=True)
 def plot_heatmap(matrix: str, output: str, bed_files: Union[list, tuple]) -> None:
     labels = [Path(file).name.split(".")[0] for file in bed_files]
     labels = " ".join(labels)
     command = f"plotHeatmap -m {matrix} --regionsLabel {labels} --refPointLabel CpG --dpi 500 --perGroup -o {output}"
-    
+
     print(f"Running: {command}")
     call(command, shell=True)
 
+
+@task(log_prints=True)
+def plot_PCA(summary_matrix: str, output: str) -> None:
+    command = f"plotPCA -in {summary_matrix} -o {join(output, 'pca.png')}"
+
+    print(f"Running: {command}")
+    call(command, shell=True)
+
+
 @flow(log_prints=True)
-def start_analysis(bed_files: Union[list, tuple], encode_data_directory: str, window: int, output: str, workers: int) -> None:
+def start_analysis(
+    bed_files: Union[list, tuple],
+    encode_data_directory: str,
+    window: int,
+    output: str,
+    workers: int,
+) -> None:
     bigwig_files = glob(join(encode_data_directory, "data", "*.bigWig"))
     if not bigwig_files:
-        raise DataNotFound(f"Not found bigWig files in data/ in {encode_data_directory} directory!")
-    
+        raise DataNotFound(
+            f"Not found bigWig files in data/ in {encode_data_directory} directory!"
+        )
+
     sample_sheet = join(encode_data_directory, "sample_sheet.csv")
     if not exists:
-        raise DataNotFound(f"Samplesheet not found in {encode_data_directory} directory!")
-    
+        print(f"Sample-sheet not found in {encode_data_directory}!")
+        raise DataNotFound
+
     if not exists(output):
         print(f"Created {output} directory.")
         makedirs(output, exist_ok=True)
-    
+
     for file in bed_files:
         if not exists(file):
-            raise DataNotFound(f"Not found {file} directory!")
-    
+            print(f"Not found file {file}!")
+            raise DataNotFound
+
     print("Seems to be ok.")
     sample_sheet = pd.read_csv(sample_sheet)
-    
+
     compute_matrix(sample_sheet, bigwig_files, bed_files, window, output, workers)
     plot_heatmap(join(output, "matrix"), join(output, "heatmap.png"), bed_files)
     build_summary_matrix(bigwig_files, bed_files, output, workers)
+    plot_PCA(join(output, "summary.npz"), output)
